d6895a402e0d145f7aec09e0fd30b0a8f2b07121
fariz <fariz.wisnu@bluebirdgroup.com>
2023-02-23 20:00:00 +0700

    base model

--- app/domain/dao/base_model.go
+++ app/domain/dao/base_model.go
@@ -10,20 +10,3 @@ type BaseModel struct {
 	UpdatetAt time.Time      `gorn:"->:false;column:update_at" json:"-"`
 	DeleteAt  gorm.DeletedAt `gorm:"->:false;column:delete_at" json:"-"`
 }
-
-type Role struct {
-	ID   int    `gorm:"column:id; primary_key; not null" json:"id"`
-	Role string `gorm:"column:role" json:"role"`
-	BaseModel
-}
-
-type User struct {
-	ID       int    `gorm:"column:id; primary_key; not null" json:"id"`
-	Name     string `gorm:"column:name" json:"name"`
-	Email    string `gorm:"column:email" json:"email"`
-	Password string `gorm:"column:password;->false" json:"-"`
-	Status   int    `gorm:"column:status" json:"status"`
-	RoleID   int    `gorm:"column:role_id; not null" json:"role_id"`
-	Role     Role   `gorm:"foreignKey:RoleID;references:ID" json:"role"`
-	BaseModel
-}
